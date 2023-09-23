63a45da999ac115a7e090ecf5476ee252e2a6fde
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-09-23 20:32:41 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1;
+let accountEmail = "anc@gmail.com";
+var accountPassword = "12345";
+accountCity = "PATNA";  //directly writing variable name
+
+//accountId=2; //not allowed as it is assigned using const
+accountEmail = "zxc@yahoo.com";
+accountPassword = "12112121";
+accountCity = "Bangalore";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
