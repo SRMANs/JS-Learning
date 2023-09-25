11cf786a6b8671abb2ba3bf7c6ea6292d3375721
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-09-25 10:18:40 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33abc";
+let value = undefined;
+let valInNo = Number(score)
+let val = Number(value)
+
+console.log(typeof score)
+console.log(typeof valInNo)
+console.log(score)
+console.log(valInNo) //gets converted into a number but displays NaN.
+console.log(value)
+console.log(typeof val)
+console.log(val)
+
+//"33"=>33
+//"33abc" => NaN
+//true=>1; false=>0
+//"" => false; "hsjdvfj" => true
+//1 => true; 0 => false
