c967f6bd43badfa07dc55a43a95881cf4192402a
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-09-25 09:28:41 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+//Data Types
+//number
+//bigint
+//string => ""
+//boolean
+//null
+//undefined
+//symbol
+
+console.log(typeof undefined)
+console.log(typeof null) //null is an object type
+console.log(typeof string)
+console.log(typeof number)
