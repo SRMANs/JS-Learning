fc77c20a2c3923b4b7ea4d4fa032650117b08a0b
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-09-26 06:40:35 +0000

    All data types understood

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,7 +7,29 @@
 //undefined
 //symbol
 
-console.log(typeof undefined)
-console.log(typeof null) //null is an object type
-console.log(typeof string)
-console.log(typeof number)
+// console.log(typeof undefined)
+// console.log(typeof null) //null is an object type
+// console.log(typeof string)
+// console.log(typeof number)
+
+//Primitive Data Types: Direct reference to memory is not allocated to these. We get a copy of variables associated with thses data types. 
+//string, number, boolean, null, undefined (defined a variable, memory is allocated to it but you have not assigned any value yet), symbol, bigint
+
+//Reference (Non Primitive)
+//Array, Objects, Functions
+
+const id = Symbol("123");
+const bigNumber = 54536873465866359863n;
+const score = 873;
+const scoreVal = 100.35
+const temp = null;
+let a;
+const arr = ["suraj",'ranjan','rajiv']
+
+console.log(typeof bigNumber);
+console.log(typeof a);
+console.log(typeof temp);
+console.log(typeof scoreVal);
+console.log(typeof score);
+console.log(typeof id);
+console.log(typeof arr);
