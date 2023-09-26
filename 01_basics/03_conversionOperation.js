c6a044e2b6d38e7295004de158f90c99a5ad1b8c
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-09-26 05:54:06 +0000

    Comparison operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,16 +3,28 @@ let value = undefined;
 let valInNo = Number(score)
 let val = Number(value)
 
-console.log(typeof score)
-console.log(typeof valInNo)
-console.log(score)
-console.log(valInNo) //gets converted into a number but displays NaN.
-console.log(value)
-console.log(typeof val)
-console.log(val)
+// console.log(typeof score)
+// console.log(typeof valInNo)
+// console.log(score)
+// console.log(valInNo) //gets converted into a number but displays NaN.
+// console.log(value)
+// console.log(typeof val)
+// console.log(val)
 
 //"33"=>33
 //"33abc" => NaN
 //true=>1; false=>0
 //"" => false; "hsjdvfj" => true
 //1 => true; 0 => false
+
+
+//************************ OPERATIONS ***********************
+
+console.log(+true);
+console.log(+"")
+
+
+//************************* COMPARISON *********************** 
+//=== strict check (It also checks the data type of variable )
+console.log("2" === 2)
+console.log("2" == 2)
