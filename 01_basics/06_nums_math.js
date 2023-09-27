c8aa9be65966a488a4ae0d809bd08b966c1f1b6e
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-09-27 04:20:50 +0000

    num and math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,13 @@
+const a = 142.49;
+// console.log(a.toPrecision(3));
+
+//********************** MATH ********************
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.50)); //floor(), ceil(), min(), max() etc
+// console.log(Math.min(2, 4, 7, 11))
+
+console.log(Math.random()); //Value always lie between 0 and 1 by default
+console.log((Math.random()*10) + 1);
+const min = 10;
+const max = 20;
+console.log((Math.random() * (max-min+1))+min);
