e71c32687e2c8fd25cf2a05210e535030e76754e
SRMANs <86324879+SRMANs@users.noreply.github.com>
2023-10-03 13:09:14 +0000

    Arrow functions

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -5,16 +5,29 @@ const user = {
     price : 299,
 
     welcomeMessage: function(){
-        console.log(`${this.username}, welcome to website`);  //if we don't give this output is username not defined
-        console.log(this);
+        // console.log(`${this.username}, welcome to website`);  //if we don't give this output is username not defined
+        // console.log(this);
     }
 }
 // user.welcomeMessage()
 // user.username = "Ranjan"
 // user.welcomeMessage()
-console.log(this);
+// console.log(this);
 
 function chai(){
-    console.log(this);
+    // console.log(this);
 }
 chai()
+
+//****************************************************************************************
+
+// const addTwo = (num1, num2) => {
+//     return num1+num2 //In explicit return we use return keyword
+// }
+
+const addTwo = (num1, num2) => num1+num2 //This implicitly returns sum. No need to write the return keyword. We can wrap (num1+num2) in parentheses.
+//Do not use curly braces in place of parentheses else will have to use return keyword. 
+console.log(addTwo(3, 4));
+
+const addTo = (num1, num2) => ({userName: 'Suraj'})  //Objects need to be written in parentheses.
+console.log(addTo(3, 4));
